79074377bb0c7f6a0035b05a39374f7563bafdb9
Ramsey Jordan <ramsey.jordan.84@gmail.com>
2018-10-27 23:45:37 -0400

    added function

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove Task event
+  taskList.addEventListener('click', removeTask);
+  // Clear Task even
+  clearBtn.addEventListener('click', clearTasks);
 
 }
 
@@ -29,7 +33,45 @@ function addTask(e) {
 function addTask(e){
   if(taskInput.value === ' '){
     alert('Add a Task')
-  }
+  } else {
+
+  // Create Li 
+  const li = document.createElement('li')
+  // Add Class
+  li.className = 'collection-item';
+  // Create text node and append to the Li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create New Link element
+  const link = document.createElement('a');
+  // Add Class
+  link.className = 'delete-item secondary-content';
+  // Add Icon html
+  link.innerHTML = '<i class="fas fa-times"></i>';
+  // Append the Link to the li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear Input
+  taskInput.value = ' ';
 
   e.preventDefault();
+  }
+}
+
+// Remove Task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks() {
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 }
